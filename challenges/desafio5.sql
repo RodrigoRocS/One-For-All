9890322cf0a73cce5b3414f63c84428d73141302
RodrigoRocS <rrsilva1701@gmail.com>
2023-06-07 01:43:23 -0300

    task 7

--- challenges/desafio5.sql
+++ challenges/desafio5.sql
@@ -2,6 +2,6 @@ SELECT m.nome_musica AS cancao, COUNT(*) AS reproducoes
 FROM SpotifyClone.musicas AS m
 JOIN SpotifyClone.historico_reproducoes AS h ON m.id_musica = h.id_musica
 GROUP BY m.nome_musica
-ORDER BY reproducoes DESC, cancao ASC
+ORDER BY reproducoes DESC, cancao
 LIMIT 2;
 
---
+++ challenges/desafio7.sql
@@ -0,0 +1,9 @@
+SELECT
+  a.nome_artista AS artista,
+  al.nome_album AS album,
+  COUNT(s.id_pessoa_usuaria) AS pessoas_seguidoras
+FROM SpotifyClone.artistas AS a
+JOIN SpotifyClone.albuns AS al ON a.id_artista = al.id_artista
+LEFT JOIN SpotifyClone.seguindo_artistas AS s ON a.id_artista = s.id_artista
+GROUP BY a.id_artista, al.id_album
+ORDER BY pessoas_seguidoras DESC, artista, album;
