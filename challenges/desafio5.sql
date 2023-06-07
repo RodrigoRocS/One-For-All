23e7da853e18f72ef8de532bbcd80ec86a1c45c7
RodrigoRocS <rrsilva1701@gmail.com>
2023-06-07 01:24:07 -0300

    task 5

---
+++ challenges/desafio5.sql
@@ -0,0 +1,7 @@
+SELECT m.nome_musica AS cancao, COUNT(*) AS reproducoes
+FROM SpotifyClone.musicas AS m
+JOIN SpotifyClone.historico_reproducoes AS h ON m.id_musica = h.id_musica
+GROUP BY m.nome_musica
+ORDER BY reproducoes DESC, cancao ASC
+LIMIT 2;
+
